f7feb94a7d6e08def35d66e57a9c0d4b7dbe0bd2
Biggie Cheese <Jv3studios@gmail.com>
2024-11-29 18:22:07 -0600

    Working on settup up the env

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import './App.css'
+import "./styles/app.scss";
+import HomePage from "./pages/homepage";
 
 function App() {
   return (
     <>
       <h1>Hello World!</h1>
+      <HomePage />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
